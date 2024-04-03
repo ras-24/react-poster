389f1b6cd5d425d5588281c3695b51244dab60fa
ras-24 <siregar.riyando@gmail.com>
2024-04-03 14:26:00 +0800

    Adding Modal Component

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,15 +1,33 @@
-import Post from './Post';
-import NewPost from './NewPost';
-import classes from './PostsList.module.css';
+import { useState } from "react";
+import Post from "./Post";
+import NewPost from "./NewPost";
+import Modal from './Modal';
+import classes from "./PostsList.module.css";
 
 function PostsList() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-    <NewPost />
-    <ul className={classes.posts}>
-      <Post author="Ras" body="React.js is awesome!" />
-      <Post author="Ros" body="Check out the Course!" />
-    </ul>
+      <Modal>
+        <NewPost
+          onBodyChange={bodyChangeHandler}
+          onAuthorChange={authorChangeHandler}
+        />
+      </Modal>
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Ros" body="Check out the Course!" />
+      </ul>
     </>
   );
 }
