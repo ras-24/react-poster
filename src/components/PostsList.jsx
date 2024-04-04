b5165d2da6361810a07e8d53f84155fc19e547d9
ras-24 <siregar.riyando@gmail.com>
2024-04-04 11:05:43 +0800

    Outputting List Data

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,19 +6,34 @@ import Modal from "./Modal";
 import classes from "./PostsList.module.css";
 
 function PostsList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  }
+
   return (
     <>
       {isPosting ? (
         <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} />
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       ) : (
         false
       )}
-
-      <ul className={classes.posts}>
-        <Post author="Ros" body="Check out the Course!" />
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   );
 }
