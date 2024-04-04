84efb2421a043668de7e4cecf947d41dca942d0c
ras-24 <siregar.riyando@gmail.com>
2024-04-04 10:39:33 +0800

    Handling Form Submission for NewPost Component

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,32 +5,18 @@ import NewPost from "./NewPost";
 import Modal from "./Modal";
 import classes from "./PostsList.module.css";
 
-function PostsList({isPosting, onStopPosting}) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
+function PostsList({ isPosting, onStopPosting }) {
   return (
     <>
       {isPosting ? (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-            onCancel={onStopPosting}
-          />
+          <NewPost onCancel={onStopPosting} />
         </Modal>
-      ) : false}
+      ) : (
+        false
+      )}
 
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Ros" body="Check out the Course!" />
       </ul>
     </>
