cd8d82cf85197553f07b5fc4d490584063975526
ras-24 <siregar.riyando@gmail.com>
2024-04-05 16:12:23 +0800

    Fixing Linking & Navigating Component

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import App from './App';
-import NewPost from './components/NewPost';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Posts from "./routes/Posts";
+import NewPost from "./routes/NewPost";
+import RootLayout from "./routes/RootLayout";
+import "./index.css";
 
 const router = createBrowserRouter([
-  { path: '/', element: <App />},
-  { path: '/create-post', element: <NewPost />}
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Posts />,
+        children: [{ path: "/create-post", element: <NewPost /> }],
+      },
+    ],
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
